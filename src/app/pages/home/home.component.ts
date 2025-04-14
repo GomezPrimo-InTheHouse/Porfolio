2816e620be1a4284722be89418e8e045aeea8c42
GomezPrimo-InTheHouse <juliangomezprimo@gmail.com>
2025-04-14 00:05:55 -0300

    avances en header

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
+import { DashboardComponent } from '../../components/dashboard/dashboard.component';
+
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent, DashboardComponent],
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule],
   standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
+ 
+
 export class HeaderComponent {
 
 }
